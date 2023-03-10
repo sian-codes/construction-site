bdaf804b5024ee090ea752b5f8fe643072a9df45
Siân <66971939+sian-codes@users.noreply.github.com>
2023-03-10 21:55:40 +0000

    refactored site to include Home page, Blog page and created a Header with working Links

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { thin } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+
+import "./App.css";
+import UnderConstruction from "./pages/UnderConstruction";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <div className="fade-in">
-          <h1>Site Under Construction</h1>
-          <div className="buttons">
-              <a className="btn-contact" href="mailto:sian@siancodes.com" target="_blank"><FontAwesomeIcon icon={thin('envelope')}/> Contact Me</a>
-              <a className="coffee" href="https://siancodes.com" ><FontAwesomeIcon icon={thin("cup-togo")} /></a>
-              <a className="btn-link" href="https://github.com/sian-codes" target="_blank">GitHub <FontAwesomeIcon icon={thin("code")}/></a>
-          </div>
-    </div>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<UnderConstruction />} />
+                {/*<Route path="/blog" element={<Blog />} />*/}
+                {/*<Route path="/home" element={<Home />} />*/}
+                           </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import "./home.css";
+import PageHeader from "../components/PageHeader";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <PageHeader></PageHeader>
+        </div>
+
+    )
+};
